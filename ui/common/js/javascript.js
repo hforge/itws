2837f7229c8c171e09a6ed2dc422dca047898a8d
Taverne Sylvain <sylvain@itaapy.com>
2010-10-25 12:50:43 +0200

    [Migration 0.62] Fix fancybox

--- ui/common/js/javascript.js
+++ ui/common/js/javascript.js
@@ -24,7 +24,7 @@ function show_more_news(link) {
 
 /* Admin bar */
 $(document).ready(function() {
-  $("a[rel='fancybox']").click(function(e){
+  $(".admin-icons-bar a[rel='fancybox']").click(function(e){
     $.fancybox(
       {'type': 'iframe',
        'transitionIn': 'none',
