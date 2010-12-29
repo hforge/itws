86777e17f7d95ec6d0473cf79036656942d3c0d1
Henry Obein <henry@itaapy.com>
2010-12-29 12:42:39 +0100

    feed_views: Exclude internal use resources from Search_View results

--- feed_views/collection.py
+++ feed_views/collection.py
@@ -15,11 +15,15 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Import from itools
-from itools.database import PhraseQuery
+from itools.database import NotQuery, OrQuery, PhraseQuery
 from itools.gettext import MSG
+
+# Import from ikaaro
+from ikaaro.utils import get_base_path_query
 
 # Import from itws
 from base import Feed_View
+
 
 
 class Details_View(Feed_View):
@@ -37,11 +41,13 @@ class Details_View(Feed_View):
 
 
     def get_content_namespace(self, resource, items, context):
-        namespace = Feed_View.get_content_namespace(self, resource, items, context)
+        proxy = super(Details_View, self)
+        namespace = proxy.get_content_namespace(resource, items, context)
         namespace['thumb_width'] = 90
         namespace['thumb_height'] = 90
         namespace['more_title'] = MSG(u'Read more')
         return namespace
+
 
 
 class Search_View(Feed_View):
@@ -52,12 +58,29 @@ class Search_View(Feed_View):
     styles = ['/ui/feed_views/search_view.css']
     content_template = '/ui/feed_views/search_view.xml'
 
+    def get_items(self, resource, context, *args):
+        args = list(args)
+        # Exclude internal use resources
+        method = getattr(resource, 'get_internal_use_resource_names', None)
+        if method:
+            exclude_query = []
+            resource_abspath = resource.get_abspath()
+            for name in method():
+                abspath = resource_abspath.resolve2(name)
+                q = get_base_path_query(str(abspath), include_container=True)
+                exclude_query.append(q)
+
+            args.append(NotQuery(OrQuery(*exclude_query)))
+        return Feed_View.get_items(self, resource, context, *args)
+
+
 
 class DetailsWithoutPicture_View(Feed_View):
 
     view_name = 'details-without-picture-view'
     view_title = MSG(u'Details View (withour pictures)')
     content_template = '/ui/news/SectionNews_view.xml'
+
 
 
 class Title_View(Feed_View):
